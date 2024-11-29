b378557d2a5b6d7230f82a1c66b2168b61d6648b
Shubhhh1243 <shubham.khaire@atmstech.in>
2024-11-29 12:31:40 +0530

    responsiveness added

--- app/urls.py
+++ app/urls.py
@@ -4,9 +4,8 @@ from app.views import *
 urlpatterns = [
     path("", sign_in, name="sign_in"),
     path("register/", register, name="register"),
-    path("company-registration/", company_registration, name="company-registration"),
-    path("upload-preview/", upload_and_preview, name="upload-preview"),
-    path("list-view/", list_view, name="list-view"),
+    path("customer-registration/", company_registration, name="customer-registration"),
+    path("products/", list_view, name="products"),
     path("users/", users, name="users"),
     path("history/", history, name="history"),
     path("history2/", history2, name="history2"),
@@ -14,8 +13,8 @@ urlpatterns = [
     path("upload-appreal/", upload_appreal, name="upload-appreal"),
     path("emailer/", emailer, name="emailer"),
     # path('upload_image',upload_image,name='upload_image'),
-    path('list_image', list_image, name='list-image'),
+    path('list_image/', list_image, name='list-image'),
     path("user-setting/", user_setting, name="user-setting"),
     path("email-template/", email_template, name="email-template"),
-    path("upload-stuff/", upload_stuff, name="upload-stuff"),
+    path("view-appreal/", upload_stuff, name="view-appreal"),
 ]
